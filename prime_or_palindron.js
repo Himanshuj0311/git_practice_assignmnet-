4c43b68641480b646f5c7da95a3efe9df05451aa
Himanshuj0311 <himanshu.masai@gmail.com>
2022-09-14 19:55:06 +0530

    prime_or_palindron

---
+++ prime_or_palindron.js
@@ -0,0 +1,18 @@
+let num=3;
+let factor=0;
+for(let i=1;i<num;i++){
+if(num%i==0){
+factor++}}
+if(factor==2){
+console.log("Yes");}
+else{console.log("No");}
+
+
+
+let str="madam"
+let bag="";
+for(let i=0;i<str.length;i++){
+bag+=str[i];}
+if(bag==str[i]){
+console.log("Yes");}
+else{console.log("No");}
